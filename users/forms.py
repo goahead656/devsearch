86c9adaec5797b420153d66a0ac6aec7f5abe279
xxx <2033897550@qq.com>
2024-07-26 21:12:20 +0800

    add register page and style login,register page

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,27 @@
 from django.forms import ModelForm
 from .models import User
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'email',
+            'username',
+            'password1',
+            'password2'
+            ]
+        labels = {
+            'first_name':'Name',
+        }
+
+    # change input style
+    def __init__(self,*args,**kwargs):
+        super(CustomUserCreationForm,self).__init__(*args,**kwargs)
+
+        for name,field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
+
--- users/views.py
+++ users/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import login,authenticate,logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profiles
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
 def loginPage(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('profiles')
@@ -28,13 +31,34 @@ def loginPage(request):
             return redirect('profiles')
         else:
             messages.error(request,'Username OR password is incorrect')
-
-    return render(request,'users/login_register.html')
+    context = {'page':page}
+    return render(request,'users/login_register.html',context)
 
 def logoutUser(request):
     logout(request)
     messages.error(request,'Username does not exist')
     return redirect('login')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request,'User account was created!')
+            login(request,user)
+            return redirect('profiles')
+
+        else:
+            messages.error(request,'An error has occurred during registration')
+
+    context = {'page':page,'form':form}
+    return render(request,'users/login_register.html',context)
 
 def profiles(request):
     profiles = Profiles.objects.all()
