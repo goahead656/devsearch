23ad43666b7048c6da348133fab36a181c2c77ec
xxx <2033897550@qq.com>
2024-08-03 14:29:40 +0800

    set more serializer model and add extra attrabute

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,36 @@
 from rest_framework import serializers
-from projects.models import Project
+from projects.models import Project,Tag,Review
+from users.models import Profiles
 
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profiles
+        fields = '__all__'
+        
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
 
 class ProjectSerializer(serializers.ModelSerializer):
+    # override some attribute
+    owner = ProfileSerializer(many=False)
+    tags = TagSerializer(many=True)
+    # this is another way to use this model
+    reviews = serializers.SerializerMethodField()
+    
     class Meta:
         model = Project
         fields = '__all__'
         
+    # get_reviews is correlated with reviews
+    def get_reviews(self,obj):
+        reviews = obj.review_set.all()
+        serializer = ReviewSerializer(reviews,many=True)
+        return serializer.data
